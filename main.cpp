cdb5b71e299892e2d453c50a09443d2b6f83fa0a
rakh1213 <rail.khamitov@netcracker.com>
2020-01-26 22:22:26 +0400

    clear files

--- main.cpp
+++ main.cpp
@@ -1,27 +1,73 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 void drawLine(int x0, int y0, int x1, int y1, TGAImage &tgaImage, TGAColor color)
 {
-	for (float i = 0.; i < 1.; i += .0001)
+
+	bool transpose = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1))
 	{
-		int x = x0 * (1. - i) + x1 * i;
-		int y = y0 * (1. - i) + y1 * i;
-		tgaImage.set(x, y, color);
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		transpose = true;
+	}
+
+	if (x0 > x1)
+	{
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	float derror2 = std::abs(dy) * 2;
+	float error2 = 0;
+	int y = y0;
+	for (float x = x0; x < x1; x++)
+	{
+		if (transpose)
+		{
+			tgaImage.set(y, x, color);
+		}
+		else
+		{
+			tgaImage.set(x, y, color);
+		}
+		error2 += derror2;
+		if (error2 > dx)
+		{
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx*2;
+		}
 	}
 }
 
 int main(void)
 {
+	model = new Model("obj/african_head.obj");
 	TGAImage image(100, 100, TGAImage::RGB);
-	drawLine(13, 20, 80, 40, image, white);
-	drawLine(20, 13, 40, 80, image, red);
-	drawLine(80, 40, 13, 20, image, red);
+	for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            drawLine(x0, y0, x1, y1, image, white);
+        }
+    }
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
+	image.write_tga_file("output1.tga");
 	return 0;
 }
-
