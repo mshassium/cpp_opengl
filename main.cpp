cb9a7fc09838bea4edcd47e8875120b44e3d8ca2
rakh1213 <rail.khamitov@netcracker.com>
2020-02-02 03:49:49 +0400

    commit first lesson

--- main.cpp
+++ main.cpp
@@ -52,20 +52,22 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &tgaImage, TGAColor color
 
 int main(void)
 {
-	model = new Model("obj/minicouper.obj");
+	model = new Model("obj/african_head.obj");
 	TGAImage image(width, height, TGAImage::RGB);
-	for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            drawLine(x0, y0, x1, y1, image, white);
-        }
-    }
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++)
+		{
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			drawLine(x0, y0, x1, y1, image, white);
+		}
+	}
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
