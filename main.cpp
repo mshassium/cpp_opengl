97216dbbf733ab8b4f0f9030c6f56e53d1b7ac47
rakh1213 <rail.khamitov@netcracker.com>
2020-02-02 04:15:42 +0400

    lesson 2 half

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model *model = NULL;
-const int width = 800;
-const int height = 800;
+const int width = 200;
+const int height = 200;
 
 void drawLine(int x0, int y0, int x1, int y1, TGAImage &tgaImage, TGAColor color)
 {
@@ -50,25 +51,47 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &tgaImage, TGAColor color
 	}
 }
 
-int main(void)
+void drawTriangleLine(Vec2i t0, Vec2i t1, TGAImage &image, TGAColor color)
 {
-	model = new Model("obj/african_head.obj");
-	TGAImage image(width, height, TGAImage::RGB);
-	for (int i = 0; i < model->nfaces(); i++)
+	drawLine(t0.x, t0.y, t1.x, t1.y, image, color);
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+{
+	if (t0.y > t1.y)
+		std::swap(t0, t1);
+	if (t0.y > t2.y)
+		std::swap(t0, t2);
+	if (t1.y > t2.y)
+		std::swap(t1, t2);
+
+	int total_height = t2.y - t0.y;
+	for (int i = 0; i < total_height; i++)
 	{
-		std::vector<int> face = model->face(i);
-		for (int j = 0; j < 3; j++)
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+		float alpha = (float)i / total_height;
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+		if (A.x > B.x)
+			std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++)
 		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			drawLine(x0, y0, x1, y1, image, white);
+			image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
 		}
 	}
+}
 
+int main(void)
+{
+	TGAImage image(width, height, TGAImage::RGB);
+	Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+	Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+	triangle(t0[0], t0[1], t0[2], image, red);
+	triangle(t1[0], t1[1], t1[2], image, white);
+	triangle(t2[0], t2[1], t2[2], image, green);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	delete model;
